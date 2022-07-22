0ec2865292cb4173c79cf6fa99215ca5e26f82cd
paulaorpella <paulaorpella@gmail.com>
2022-07-21 22:09:45 -0300

    Correcciones front end

---
+++ Portfolio_Paula_Orpella/BackEnd/src/main/java/com/portfolio/paulaorpella/PaulaorpellaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.paulaorpella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaulaorpellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaulaorpellaApplication.class, args);
+	}
+
+}
---
+++ Portfolio_Paula_Orpella/BackEnd/src/test/java/com/portfolio/paulaorpella/PaulaorpellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.paulaorpella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulaorpellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
