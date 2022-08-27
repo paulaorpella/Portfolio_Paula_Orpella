09fb963d35720dea99987a81a98b95574a61385e
paulaorpella <paulaorpella@gmail.com>
2022-08-26 21:01:20 -0300

    admin

--- Portfolio_Paula_Orpella/BackEnd/src/main/java/com/portfolio/paulaorpella/Controller/PersonaController.java
+++ Portfolio_Paula_Orpella/BackEnd/src/main/java/com/portfolio/paulaorpella/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.paulaorpella.Entity.Persona;
 import com.portfolio.paulaorpella.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,12 +25,14 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "El usuario fue creado correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/(id)")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
@@ -37,6 +40,7 @@ public class PersonaController {
     }
     
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/(id)")
     public Persona editPersona(@PathVariable Long id,
                  @RequestParam("nombre") String nuevoNombre,
